73233cce27cb9ed4a6a147d46ab85733692484af
Andrew Kumanyaev <me@zzet.org>
2014-09-17 20:25:28 +0400

    change test repo path

--- test/support/seed_helper.rb
+++ test/support/seed_helper.rb
@@ -1,6 +1,6 @@
 unless File.exists?(TEST_REPO_PATH)
   puts 'Prepare seeds'
   FileUtils.mkdir_p(SUPPORT_PATH)
-  system(*%W(git clone --bare https://github.com/gitlabhq/testme.git), chdir: SUPPORT_PATH)
+  system(*%W(git clone --bare https://gitlab.com/gitlab-org/gitlab-test.git), chdir: SUPPORT_PATH)
   system(*%W(git checkout 5937ac0a7beb003549fc5fd26fc247adbce4a52e), chdir: SUPPORT_PATH)
 end
